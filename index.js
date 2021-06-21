17ade537ed32955a0a4c3b7ffd9dbb72de7c35cf
Jigyasa-G <75614134+Jigyasa-G@users.noreply.github.com>
2021-06-21 14:30:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,12 @@ document.querySelectorAll("img")[1].setAttribute("src" , randomDice2Image);
 if (randomNumber1 > randomNumber2) {
    document.querySelector("h1").innerHTML ="🚩Player 1 wins!";
 }
- else if
-(randomNumber1 < randomNumber2) {
+ else{ 
+    if(randomNumber1 < randomNumber2) {
   document.querySelector("h1").innerHTML ="Player 2 wins!🚩";
 
   }
   else{
     document.querySelector("h1").innerHTML ="Its a draw !";
 }
+     }
